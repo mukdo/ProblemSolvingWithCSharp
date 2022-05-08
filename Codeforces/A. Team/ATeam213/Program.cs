a4208af1a5257cd009584bc4da01ea565d08c4fd
Mukdo <bmukdo@gmail.com>
2022-05-08 17:56:01 +0600

    Codeforces/A. Team/ATeam 231 no problem Solved

---
+++ Codeforces/A. Team/ATeam213/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ATeam231
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is first input line
+            string number = Console.ReadLine();
+            int count = 0;
+            for(int i = 0; i < Convert.ToInt32(number); i++)
+            {
+                string data = Console.ReadLine();
+                string[] getValue = data.Split(' ');
+                try
+                {
+                    int petya = Convert.ToInt32(getValue[0]);
+                    int vasya = Convert.ToInt32(getValue[1]);
+                    int tonya = Convert.ToInt32(getValue[2]);
+
+                    int result = petya + vasya + tonya;
+
+                    if(result > 1)
+                    {
+                        count++;
+                    }
+
+                }
+                catch(Exception)
+                {
+
+                }
+             
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
